bc0f2e710eb7a737dc57fa67d9083beef390b350
luismiguel010 <luismiguel010@gmail.com>
2021-10-05 01:31:28 -0500

    Se añade route a ofertas laborales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { OfertasLaboralesComponent } from './pages/ofertas-laborales/ofertas-laborales.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/', pathMatch: 'full'},
+  {path: '', component: InicioComponent},
+  {path: 'ofertas', component: OfertasLaboralesComponent},
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    OfertasLaboralesComponent,
+    InicioComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
