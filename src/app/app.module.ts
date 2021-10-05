6686613c79fec26e85cc7d7747d65fc3dd65b9b2
luismiguel010 <luismiguel010@gmail.com>
2021-10-05 03:37:33 -0500

    cards con info provisional

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { OfertasLaboralesComponent } from './pages/ofertas-laborales/ofertas-laborales.component';
 import { InicioComponent } from './pages/inicio/inicio.component';
+import { HojasDeVidaComponent } from './pages/hojas-de-vida/hojas-de-vida.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/', pathMatch: 'full'},
@@ -19,10 +23,13 @@ const routes: Routes = [
     HeaderComponent,
     FooterComponent,
     OfertasLaboralesComponent,
-    InicioComponent
+    InicioComponent,
+    HojasDeVidaComponent
   ],
   imports: [
     BrowserModule,
+    CarouselModule,
+    BrowserAnimationsModule,
     RouterModule.forRoot(routes)
   ],
   exports: [RouterModule],
