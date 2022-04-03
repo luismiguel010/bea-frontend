2c5d3552a7a85c7136b18286052081c57f96d358
luismiguel010 <54339559+luismiguel010@users.noreply.github.com>
2022-04-03 14:39:51 -0500

    Se añade técnico y tecnólogo a nivel académico

--- src/app/enums/nivel-academico.ts
+++ src/app/enums/nivel-academico.ts
@@ -2,6 +2,8 @@ export enum NivelAcademico {
     Escuela = "Escuela",
     Bachillerato = "Bachillerato",
     Pregrado = "Pregrado",
+    Tecnico = "Técnico",
+    Tecnologo = "Tecnólogo",
     Especialista = "Especialista",
     Posgrado = "Posgrado",
     Doctorado = "Doctorado"
